a8908e151c24feff232729d1053ffd24dcb394ee
RohitRocks44 <shrivastvarohit100@gmail.com>
2024-07-29 15:02:57 +0530

    list instances using boto3

---
+++ listec2instances.py
@@ -0,0 +1,8 @@
+import boto3
+aws_management_console=boto3.session.Session(profile_name="default",region_name="us-east-1")
+ec2_console=aws_management_console.client(service_name="ec2")
+
+result=ec2_console.describe_instances()['Reservations']
+for each_instance in result:
+    for value in each_instance['Instances']:
+        print(value['InstanceId'])
